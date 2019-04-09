7c79b74a8e1a24a390a1625280ff37a007b9fa01
kaju2759 <kajuringo72@gmail.com>
2019-04-09 22:06:56 +0900

    first commit

---
+++ src/main/java/com/tcInfo/TcinformationApplication.java
@@ -0,0 +1,13 @@
+package com.tcInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcinformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcinformationApplication.class, args);
+	}
+
+}
