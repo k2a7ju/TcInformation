ded422a921e64688c463d2bfeedbbcace9114fd8
kaju2759 <kajuringo72@gmail.com>
2019-04-09 23:26:09 +0900

    [add] service クラスの追加

---
+++ src/main/java/com/tcInfo/service/TcInfoEndpointService.java
@@ -0,0 +1,17 @@
+package com.tcInfo.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class TcInfoEndpointService {
+
+	public ResponseEntity<String> run() {
+		ResponseEntity<String> responseEntity = new ResponseEntity<String>("sample",HttpStatus.ACCEPTED);
+
+		return responseEntity;
+	}
+
+}
