8c3f7ad1c89e2441ca88a42d47eb739931934287
kaju2759 <kajuringo72@gmail.com>
2019-04-11 22:39:36 +0900

    [add] ArticleRepository クラスの追加

---
+++ src/main/java/com/tcInfo/repository/ArticleRepository.java
@@ -0,0 +1,24 @@
+package com.tcInfo.repository;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import com.tcInfo.entity.ArticleEntity;
+
+public class ArticleRepository {
+	public List<ArticleEntity> read(String fileName){
+		Reader reader = null;
+		try {
+			reader = Files.newBufferedReader(Paths.get(fileName));
+		} catch (IOException e) {
+			// TODO: Exception: throw ファイルが存在しません。
+		}
+        CsvToBean<ArticleEntity> csvToBean = new CsvToBeanBuilder<ArticleEntity>(reader).withType(ArticleEntity.class).build();
+        return csvToBean.parse();
+    }
+}
