5ffa25d6027e1f8f28d03f2326bf71503dbdb017
kaju2759 <kajuringo72@gmail.com>
2019-04-11 23:51:11 +0900

    [add] TcInfoControllerAdvice クラスの追加

---
+++ src/main/java/com/tcInfo/endpoint/TcInfoControllerAdvice.java
@@ -0,0 +1,50 @@
+package com.tcInfo.endpoint;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import com.tcInfo.bean.RequestBean;
+
+@ControllerAdvice(assignableTypes = TcInfoEndpoint.class)
+public class TcInfoControllerAdvice {
+
+	@ModelAttribute
+	public RequestBean attribute(HttpServletRequest request, @RequestHeader HttpHeaders httpHeaders,
+            @RequestBody String body) {
+		RequestBean requestBean = new RequestBean();
+
+		// id を RequestBean へ追加
+		String id = httpHeaders.getFirst("id");
+		if (id == null) {
+			// TODO: Exception id が null
+		}
+
+		// password を RequestBean へ追加
+		String password =  httpHeaders.getFirst("password");
+		if (password == null) {
+			// TODO: Exception password が null
+		}
+
+		// contentType を RequestBean へ追加
+		String contentType = httpHeaders.getContentType().toString();
+		if (contentType == null || !contentType.equals(MediaType.APPLICATION_JSON.toString())) {
+			// TODO: Exception content-type が不正
+		}
+		requestBean.setContentType(contentType);
+
+		if (body == null) {
+			// TODO: Exception body が空
+		}
+		requestBean.setBody(body);
+
+
+		return requestBean;
+	}
+
+}
