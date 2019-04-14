5a27e91335889187207d338aa052e6b613e5bd68
kaju2759 <kajuringo72@gmail.com>
2019-04-14 16:13:38 +0900

    [mod] APIのエンドポイントを変更

--- src/main/java/com/tcInfo/endpoint/TcInfoEndpoint.java
+++ src/main/java/com/tcInfo/endpoint/TcInfoEndpoint.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tcInfo.bean.RequestBean;
 import com.tcInfo.service.TcInfoEndpointService;
 
 @RestController
@@ -13,11 +14,9 @@ public class TcInfoEndpoint {
 	@Autowired
     TcInfoEndpointService endpointService;
 
-	@RequestMapping(path = "/**", method = RequestMethod.GET)
-    public ResponseEntity<String> getController() {
-        ResponseEntity<String> response = this.endpointService.exec();
+	@RequestMapping(path = "/tcinfo/getArticle", method = RequestMethod.POST)
+    public ResponseEntity<String> getEndpointService(RequestBean requestBean) {
+        ResponseEntity<String> response = this.endpointService.exec(requestBean);
         return response;
     }
-
-
 }
