25a1a8d0aea7568f6b166b844f42cf6439e15af5
kaju2759 <kajuringo72@gmail.com>
2019-04-14 17:23:52 +0900

    [mod] リクエストのJSONを取得できるように変更

---
+++ src/main/java/com/tcInfo/bean/TcInfoJsonBean.java
@@ -0,0 +1,22 @@
+package com.tcInfo.bean;
+
+public class TcInfoJsonBean {
+	private String lang;
+	private String category;
+
+	/**
+	 * lang の getter
+	 * @return lang
+	 */
+	public String getLang() {
+		return lang;
+	}
+
+	/**
+	 * category の getter
+	 * @return category
+	 */
+	public String getCategory() {
+		return category;
+	}
+}
--- src/main/java/com/tcInfo/service/TcInfoEndpointService.java
+++ src/main/java/com/tcInfo/service/TcInfoEndpointService.java
@@ -1,15 +1,45 @@
 package com.tcInfo.service;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tcInfo.bean.RequestBean;
+import com.tcInfo.bean.TcInfoJsonBean;
+import com.tcInfo.repository.ArticleRepository;
 
 @Service
-public class TcInfoEndpointService {
+public class TcInfoEndpointService extends EndpointService {
 
-	public ResponseEntity<String> exec() {
-		ResponseEntity<String> responseEntity = new ResponseEntity<String>("sample",HttpStatus.ACCEPTED);
+	@Autowired
+	ArticleRepository articleRepository;
+
+	@Autowired
+	ScriptService scriptService;
+
+	public ResponseEntity<String> exec(RequestBean requestBean) {
+
+		if (requestBean == null) {
+			// TODO: Exception requestBean が null
+		}
+		String jsonText = requestBean.getBody();
+		ObjectMapper objectMapper = new ObjectMapper();
+		TcInfoJsonBean jsonBean = null;
+		try {
+			jsonBean = objectMapper.readValue(jsonText, TcInfoJsonBean.class);
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		System.out.println(jsonBean.getLang());
+
+//		String commandResult = scriptService.execCommand(command);
+//		System.out.println(commandResult);
+		ResponseEntity<String> responseEntity = new ResponseEntity<String>(requestBean.getBody(),HttpStatus.ACCEPTED);
 
 		return responseEntity;
 	}
