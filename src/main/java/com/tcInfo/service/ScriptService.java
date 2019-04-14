a37626f2fe97b8f70f24d5cff90dda938fa5a224
kaju2759 <kajuringo72@gmail.com>
2019-04-14 18:07:30 +0900

    [mod] InputStreamからString変換をメソッド化

--- src/main/java/com/tcInfo/service/ScriptService.java
+++ src/main/java/com/tcInfo/service/ScriptService.java
@@ -1,7 +1,9 @@
 package com.tcInfo.service;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import org.springframework.stereotype.Service;
 
@@ -12,17 +14,32 @@ public class ScriptService {
 		String result = null;
 		try {
 	        Runtime runtime = Runtime.getRuntime();
-	        Process process = runtime.exec("/Users/kaju/.rbenv/shims/ruby /Users/kaju/WorkSpace/eclipse_work/Tcinformation/script/scraping.rb startups");
+	        Process process = runtime.exec(command);
 	        process.waitFor();
 	        InputStream inputStream = process.getInputStream();
-	        result = inputStream.toString();
+	        result = this.inputStreemToString(inputStream);
 	    } catch (IOException | InterruptedException ex) {
 	    	// TODO: Exception: 実行時エラー
 	    }
 		return result;
 	}
 
-	public String buildCommand() {
-		return null;
+	public String buildCommand(String category) {
+		String command = "/Users/kaju/.rbenv/shims/ruby /Users/kaju/WorkSpace/eclipse_work/Tcinformation/script/scraping.rb " + category;
+		System.out.println(command);
+		return command;
 	}
+
+	private String inputStreemToString(InputStream in) throws IOException{
+
+        BufferedReader reader =
+            new BufferedReader(new InputStreamReader(in, "UTF-8"));
+        StringBuffer buf = new StringBuffer();
+        String str;
+        while ((str = reader.readLine()) != null) {
+                buf.append(str);
+                buf.append("\n");
+        }
+        return buf.toString();
+    }
 }
