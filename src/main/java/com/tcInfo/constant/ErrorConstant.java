50a5c3d752990b628901153b3627fc619884f2b9
kaju2759 <kajuringo72@gmail.com>
2019-04-26 21:00:09 +0900

    [mod] エラーメッセージのフィールドに JavaDoc を追加

--- src/main/java/com/tcInfo/constant/ErrorConstant.java
+++ src/main/java/com/tcInfo/constant/ErrorConstant.java
@@ -1,8 +1,23 @@
 package com.tcInfo.constant;
 
 public class ErrorConstant {
+	/*
+	 * ハンドリングしていないエラーが発生した場合に出力するメッセージ
+	 */
 	public static final String INTERNAL_SERVER_ERROR = "Internal Server Error";
+
+	/*
+	 * 外部コマンド実行に失敗した場合に出力するメッセージ
+	 */
 	public static final String COMMAND_EXEC_ERROR = "Command Execution error";
+
+	/*
+	 * リクエストに含まれる Content-type が JSON 以外の時に出力するメッセージ
+	 */
 	public static final String CONTENT_TYPE_FAILED = "Content-type is only application/json";
+
+	/*
+	 * 記事ファイルが存在しない場合に出力するメッセージ
+	 */
 	public static final String FILE_NOT_FOUND = "File not found";
 }
