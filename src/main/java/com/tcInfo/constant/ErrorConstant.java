086a424c68505ac08dc50a99e28f06613a251b36
kaju2759 <kajuringo72@gmail.com>
2019-04-26 20:42:57 +0900

    [mod] Exception をハンドリングした時にTcInfoExceptionを投げるように変更

--- src/main/java/com/tcInfo/constant/ErrorConstant.java
+++ src/main/java/com/tcInfo/constant/ErrorConstant.java
@@ -3,4 +3,5 @@ package com.tcInfo.constant;
 public class ErrorConstant {
 	public static final String INTERNAL_SERVER_ERROR = "Internal Server Error";
 	public static final String COMMAND_EXEC_ERROR = "Command Execution error";
+	public static final String CONTENT_TYPE_FAILED = "Content-type is only application/json";
 }
